7ffba82ab67a29a68db8b8b74a1877e0c83f4bd1
redbeers <71654161+redbeers@users.noreply.github.com>
2025-03-20 22:12:37 +0900

    init

---
+++ src/main/java/org/stella/allsites/AllsitesApplication.java
@@ -0,0 +1,13 @@
+package org.stella.allsites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsitesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsitesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/stella/allsites/AllsitesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stella.allsites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllsitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
